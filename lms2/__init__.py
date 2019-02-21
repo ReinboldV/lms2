ef3a5fe8443c9ae263b4f95004b536510c0f7274
Vincent Reinbold <vincent.reinbold@geeps.centralesupelec.fr>
2019-02-21 17:44:20 +0100

    automated test, requierements and init file

--- lms2/__init__.py
+++ lms2/__init__.py
@@ -1,1 +1,25 @@
+import types
 __version__ = '1.0'
+
+__all__ = ['lms2.core.expressions', 'lms2.core.models', 'lms2.core.param', 'lms2.core.time', 'lms2.core.unit',
+           'lms2.core.var', 'lms2.electric.batteries', 'lms2.electric.maingrids', 'lms2.electric.sources',
+           'lms2.electric.turbines', 'lms2.environment.pollutions', 'lms2.mechanic.gears', 'lms2.logical.hysteresis',
+           'lms2.thermal.dipoles', 'lms2.template.drahiX']
+
+from lms2.base.base_units import *
+from lms2.base.utils import *
+from lms2.core.expressions import *
+from lms2.core.models import *
+from lms2.core.param import *
+from lms2.core.time import *
+from lms2.core.units import *
+from lms2.core.var import *
+from lms2.electric.batteries import *
+from lms2.electric.maingrids import *
+from lms2.electric.sources import *
+from lms2.electric.turbines import *
+from lms2.environment.pollutions import *
+from lms2.mechanic.gears import *
+from lms2.logical.hysteresis import *
+from lms2.thermal.dipoles import *
+from lms2.template.drahiX import *
