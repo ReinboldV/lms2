0d0a477e3ebac7b39cc5153099b45b5358b457a0
Vincent Reinbold <vincent.reinbold@geeps.centralesupelec.fr>
2019-02-21 17:58:10 +0100

    adding properties to pyomo.Expression

--- lms2/core/expressions.py
+++ lms2/core/expressions.py
@@ -3,83 +3,102 @@
 Different types of objectives
 
 In order to automatically recognize different type of objectives or cost from different part of the system.
-We define several classes that heritate from pyomo.core.base.Objective.
-Such as Cost (in euro), Energy (in W.h), Prosumtion (in W.h), CO2 emmision (in kg) etc.
+We define several property for the pyomo Expression class.
+Such as tag, unit, sens, etc.
 """
 
 from pyomo.core.base import minimize, maximize
-from pyomo.core.base.expression import SimpleExpression
+from pyomo.environ import Expression
 
-expression_type = ['COST', 'PROSUMTION', 'CO2', 'ENERGY']
-units = ['euros', 'W.h', 'kg_eq', 'W.h']
-
-
-class Cost(SimpleExpression):
-    """
-    Cost Objective (euros)
-
-    Represents the cost of use in euros
-    """
-    def __init__(self, *arg, **kwargs):
-        """
-
-        :param args: rule or expr
-        """
-        super().__init__(*arg, **kwargs)
-        self.unit = 'euros'
-        self.expression_type = 'cost'
-        self.sens = minimize # sens=1
+_tags = ['COST', 'PROSUMTION', 'CO2', 'ENERGY', None]
+_units = ['euros', 'W.h', 'kg_eq', 'W.h', None]
+_conv = [minimize, maximize, None]
 
 
-class Prosumtion(SimpleExpression):
-    """
-    Prosumtion Objective (W.h)
-
-    Represents the prosumation capacity, i.e. the auto-consumtion in watt-hour
-    """
-    def __init__(self, *arg, **kwargs):
-        """
-
-        :param str name: Name of the model
-        :param args:
-        """
-        super().__init__(*arg, **kwargs)
-        self.unit = 'W.h'
-        self.expression_type = 'prosumtion'
-        self.sens = maximize  # sens=-1
+# @property
+def tag(self):
+    return self._tag
 
 
-class Energy(SimpleExpression):
-    """
-    Energy Objective (euros)
-
-    Represent the total use of energy in Watt-hour
-    """
-    def __init__(self, *arg, **kwargs):
-        """
-
-        :param str name: Name of the model
-        :param args:
-        """
-        super().__init__(*arg, **kwargs)
-        self.unit = 'W.h'
-        self.expression_type = 'energy'
-        self.sens = minimize  # sens=1
+# @tag.setter
+def settag(self, tag):
+    if tag in _tags:
+        self._tag = tag
+    else:
+        print('test')
+        raise Warning(f'{tag} is not a valide tag. By default available tags are : "{_tags}".')
 
 
-class CO2(SimpleExpression):
-    """
-    CO2 Objective (kg_eqco2)
+# @tag.getter
+def gettag(self):
+    return self._tag
 
-    Represents greenhouse gaz emission in kilograms of equivalent CO2.
-    """
-    def __init__(self, *arg, **kwargs):
-        """
 
-        :param str name: Name of the model
-        :param args:
-        """
-        super().__init__(*arg, **kwargs)
-        self.unit = 'kg_eq'
-        self.expression_type = 'co2'
-        self.sens = minimize  # sens=1
+# @tag.deleter
+def deltag(self):
+    if hasattr(self, '_tag'):
+        del self._tag
+
+
+doc_tag = 'tag argument is used to group the expression and helps to construct meta-expression. ' \
+          'For instance, it can be used for summing all the Expression tagged with the same tag "COST".'
+
+
+Expression.tag = property(gettag, settag, deltag, doc_tag)
+exp = Expression()
+
+
+def sens(self):
+    return self._sens
+
+
+def setsens(self, sens):
+    if sens in _conv:
+        self._sens = sens
+    else:
+        raise Warning(f'{sens} is not a valide sens. By default, available sens are {_conv}.')
+
+
+def getsens(self):
+    return self._sens
+
+
+def delsens(self):
+    if hasattr(self, '_sens'):
+        del self._sens
+
+
+doc_sens = 'sens is used to group the expression and helps to reconstruct meta-expression.' \
+           'For instance it can be used to add expression to be minimized or maximized.'
+
+Expression.sens = property(getsens, setsens, delsens, doc_sens)
+
+
+def unit(self):
+    return self._unit
+
+
+def setunit(self, unit):
+    if unit in _units:
+        self._unit = unit
+    else:
+        raise Warning(f'{unit} is not a valide unit. By default, available sens are {_units}.')
+
+
+def getsens(self):
+    return self._sens
+
+
+def delsens(self):
+    if hasattr(self, '_sens'):
+        del self._sens
+
+
+doc_sens = 'sens is used to group the expression and helps to reconstruct meta-expression.' \
+           'For instance it can be used to add expression to be minimized or maximized.'
+
+Expression.sens = property(getsens, setsens, delsens, doc_sens)
+
+
+if __name__ == '__main__':
+    pass
