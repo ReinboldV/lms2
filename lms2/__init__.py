6ccc8d3d627fbb91ddf62d0efda941fcfb2ffcd2
Vincent Reinbold <vincent.reinbold@geeps.centralesupelec.fr>
2019-05-28 17:36:10 +0200

    adding packages in init

--- lms2/__init__.py
+++ lms2/__init__.py
@@ -5,10 +5,21 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-__all__ = ['lms2.core.expressions', 'lms2.core.models', 'lms2.core.param', 'lms2.core.time', 'lms2.core.unit',
-           'lms2.core.var', 'lms2.electric.batteries', 'lms2.electric.maingrids', 'lms2.electric.sources',
-           'lms2.electric.turbines', 'lms2.environment.pollutions', 'lms2.mechanic.gears', 'lms2.logical.hysteresis',
-           'lms2.thermal.dipoles', 'lms2.template.drahiX']
+__all__ = ['lms2.core.expressions',
+           'lms2.core.models',
+           'lms2.core.param',
+           'lms2.core.time',
+           'lms2.core.unit',
+           'lms2.core.var',
+           'lms2.electric.batteries',
+           'lms2.electric.maingrids',
+           'lms2.electric.sources',
+           'lms2.electric.turbines',
+           'lms2.environment.pollutions',
+           'lms2.logical.hysteresis',
+           'lms2.mechanic.gears',
+           'lms2.template.drahix.abs_drahix',
+           'lms2.thermal.dipoles']
 
 from lms2.base.base_units import *
 from lms2.base.utils import *
@@ -16,6 +27,10 @@ from lms2.core.expressions import *
 from lms2.core.models import *
 from lms2.core.time import *
 from lms2.core.units import *
+from lms2.economic.cost import *
+from lms2.electric.abs_sources import *
+from lms2.electric.abs_batteries import *
+from lms2.electric.abs_maingrids import *
 from lms2.electric.batteries import *
 from lms2.electric.maingrids import *
 from lms2.electric.sources import *
@@ -24,4 +39,4 @@ from lms2.environment.ghg import *
 from lms2.mechanic.gears import *
 from lms2.logical.hysteresis import *
 from lms2.thermal.dipoles import *
-from lms2.template.drahiX import *
+from lms2.template.drahix.abs_drahix import *
