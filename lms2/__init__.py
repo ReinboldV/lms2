cc2a07d57654095d17b36a0833989e07b8381b14
REINBOLD Vincent <vincent.reinbold@supelec.fr>
2019-06-16 18:44:54 +0200

    minor

--- lms2/__init__.py
+++ lms2/__init__.py
@@ -7,15 +7,12 @@ del get_versions
 
 __all__ = ['lms2.core.expressions',
            'lms2.core.models',
-           'lms2.core.param',
            'lms2.core.time',
            'lms2.core.unit',
-           'lms2.core.var',
-           'lms2.electric.batteries',
-           'lms2.electric.maingrids',
-           'lms2.electric.sources',
-           'lms2.electric.turbines',
-           'lms2.environment.pollutions',
+           'lms2.electric.abs_batteries',
+           'lms2.electric.abs_maingrids',
+           'lms2.electric.abs_sources',
+           'lms2.environment.ghg',
            'lms2.logical.hysteresis',
            'lms2.mechanic.gears',
            'lms2.template.drahix.abs_drahix',
@@ -31,10 +28,6 @@ from lms2.economic.cost import *
 from lms2.electric.abs_sources import *
 from lms2.electric.abs_batteries import *
 from lms2.electric.abs_maingrids import *
-from lms2.electric.batteries import *
-from lms2.electric.maingrids import *
-from lms2.electric.sources import *
-from lms2.electric.turbines import *
 from lms2.environment.ghg import *
 from lms2.mechanic.gears import *
 from lms2.logical.hysteresis import *
