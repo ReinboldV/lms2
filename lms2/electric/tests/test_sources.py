ace03b011a961a24d2e5a88f7a5a3fbd418655cd
Vincent Reinbold <vincent.reinbold@geeps.centralesupelec.fr>
2020-02-11 18:33:26 +0100

    adding test for curtalable source

--- lms2/electric/tests/test_sources.py
+++ lms2/electric/tests/test_sources.py
@@ -10,7 +10,69 @@ class TestAbsFixedPowerSource(TestCase):
 
 
 class TestAbsScalablePowerSource(TestCase):
-    pass
+
+    def test_instanciate_curt(self):
+        from lms2 import AbsLModel
+        from lms2 import ScalablePowerSource
+
+        import pandas as pd
+        from pyomo.dae import ContinuousSet
+        from pyomo.environ import TransformationFactory
+
+        m = AbsLModel()
+        m.time = ContinuousSet()
+        m.u    = ScalablePowerSource(curtailable=True)
+
+        df = pd.Series({0: 0, 1: 1, 2: 2, 3: 1, 4: 0})
+        data_u = {
+            'time': {None: [0, 4]},
+            'profile_index': {None: df.index},
+            'profile_value': df.to_dict()}
+
+        data = \
+            {None:
+                {
+                    'time': {None: [0, 4]},
+                    'u': data_u
+                }
+            }
+        inst = m.create_instance(data)
+        TransformationFactory('dae.finite_difference').apply_to(inst, nfe=4)
+
+        self.assertTrue(hasattr(inst.u, 'p_curt'))
+
+    def test_instanciate(self):
+        from lms2 import AbsLModel
+        from lms2 import ScalablePowerSource
+
+        import pandas as pd
+        from pyomo.dae import ContinuousSet
+        from pyomo.environ import TransformationFactory
+
+        m = AbsLModel()
+        m.time = ContinuousSet()
+        m.u = ScalablePowerSource(curtailable=False)
+
+        df = pd.Series({0: 0, 1: 1, 2: 2, 3: 1, 4: 0})
+        data_u = {
+            'time': {None: [0, 4]},
+            'profile_index': {None: df.index},
+            'profile_value': df.to_dict()}
+
+        data = \
+            {None:
+                {
+                    'time': {None: [0, 4]},
+                    'u': data_u
+                }
+            }
+        inst = m.create_instance(data)
+        TransformationFactory('dae.finite_difference').apply_to(inst, nfe=4)
+
+        self.assertFalse(hasattr(inst.u, 'p_curt'))
+        self.assertTrue(hasattr(inst.u, 'p'))
+        self.assertTrue(hasattr(inst.u, 'p_scaled'))
+        self.assertTrue(hasattr(inst.u, 'scale_fact'))
 
 
 class TestAbsPowerLoad(TestCase):
@@ -28,6 +90,7 @@ class TestAbsScalablePowerLoad(TestCase):
 class TestAbsDebugSource(TestCase):
     pass
 
+
 class TestAbsPrommableLoad(TestCase):
 
     def test_instanciate_prog(self):
@@ -42,15 +105,15 @@ class TestAbsPrommableLoad(TestCase):
 
         UB = 1e6
 
-        df = pd.Series({0:0, 1:1, 2:2, 3:1, 4:0})
+        df = pd.Series({0: 0, 1: 1, 2: 2, 3: 1, 4: 0})
 
         data_prog = {
             'time': {None: [0, 10]},
-            'w1'  : {None: 3},
-            'w2'  : {None: 8},
+            'w1': {None: 3},
+            'w2': {None: 8},
             'window': {None: [2, 8]},
             'profile_index': {None: df.index},
-            'profile_value':df.to_dict()}
+            'profile_value': df.to_dict()}
 
         data = \
             {None:
@@ -65,6 +128,8 @@ class TestAbsPrommableLoad(TestCase):
 
         inst.prog.compile()
 
+
 if __name__ == '__main__':
     import unittest
+
     unittest.main()
