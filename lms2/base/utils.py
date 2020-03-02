7db2433de5eb2776aead637e528f0bf150c3ca51
Vincent Reinbold <vincent.reinbold@geeps.centralesupelec.fr>
2020-03-02 15:22:32 +0100

    adding constraint, upper and lower slack variable to pplot function

--- lms2/base/utils.py
+++ lms2/base/utils.py
@@ -6,19 +6,22 @@ Utils and tool for linearization and plots
 
 from pandas import Series
 
-__all__ = ['pplot']
+__all__ = ['pplot', 'get_slack', 'get_doc_2', 'get_duals']
 
 
 def _pplot(variable, index=None, fig=None, ax=None, **kwarg):
     """
-    Function that plots pyomo Variable or Parameter
+    Function that plots pyomo Variable, Parameter, Expression and Constraint
+
+    For Constraint, the function returns the value of the constraint's expression, the upper slack variable,
+    and the lower slack variable (if not None)
 
         :param var: Var or Param to be plotted
         :param index: New index for plotting purpose (optional)
         :param fig: figure handle (optional)
         :param ax: axes handle (optional)
         :param kwarg: any Series.plot keyword argument
-        :return:  line handle, axe handle, figure handle
+        :return:  line handle, (upper line), (lower line), axe handle, figure handle
 
     **Returns**
         - arg1 the matplotlib.pyplot.Figure handle object
@@ -39,7 +42,10 @@ def _pplot(variable, index=None, fig=None, ax=None, **kwarg):
         >>> lines = pplot(m.v, m.z, m.w, title='test', Marker='x')
     """
     import matplotlib.pyplot as plt
-    from pyomo.environ import Var, Param, Expression, value, Constraint
+    from pyomo.environ import Var, Param, Expression, Constraint
+
+    up = None
+    low = None
 
     if fig is None:
         fig = plt.figure()
@@ -73,31 +79,51 @@ def _pplot(variable, index=None, fig=None, ax=None, **kwarg):
 
     elif isinstance(variable, Expression):
         if index is None:
-            ld = Series({i: v() for i, v in variable.iteritems()} ).sort_index().plot(label=variable.name.replace('_', '\_'),
-                                                                                 fig=fig, ax=ax, **kwarg)
+            ld = Series({i: v() for i, v in variable.iteritems()}).sort_index().plot(
+                label=variable.name.replace('_', '\_'),
+                fig=fig, ax=ax, **kwarg)
         else:
-            s = Series({i: v() for i, v in variable.iteritems()} ).sort_index()
+            s = Series({i: v() for i, v in variable.iteritems()}).sort_index()
             s.index = index
             ld = s.plot(label=variable.name.replace('_', '\_'), fig=fig, ax=ax, **kwarg)
 
     elif isinstance(variable, Constraint):
         if index is None:
-            ld = Series([v() for v in variable.values()]).sort_index().plot(label=variable.name.replace('_', '\_'),
-                                                                                 fig=fig, ax=ax, **kwarg)
+            ld = Series({i: v() for i, v in variable.iteritems()}).sort_index().plot(
+                label=variable.name.replace('_', '\_'),
+                fig=fig,
+                ax=ax,
+                **kwarg)
+
+            up = Series({i: v.uslack() for i, v in variable.iteritems()}).sort_index().plot(
+                label=variable.name.replace('_', '\_')+'_uslack',
+                fig=fig,
+                color = ld.lines[-1].get_color(),
+                lineStyle = '--',
+                ax=ax,
+                **kwarg)
+
+            low = Series({i: v.lslack() for i, v in variable.iteritems()}).sort_index().plot(
+                label=variable.name.replace('_', '\_') + '_lslack',
+                fig=fig,
+                color=ld.lines[-1].get_color(),
+                lineStyle='-.',
+                ax=ax,
+                **kwarg)
+
         else:
             s = Series([v() for v in variable.values()])
             s.index = index
             ld = s.plot(label=variable.name.replace('_', '\_'), fig=fig, ax=ax, **kwarg)
 
     else:
-        raise(NotImplementedError(f'Argument "variable" must be of type Param or Var, but is actually'
-                                  f'{variable, type(variable)}'))
+        raise (NotImplementedError(f'Argument "variable" must be of type Param or Var, but is actually'
+                                   f'{variable, type(variable)}'))
 
-    return ld, ax, fig
+    return ld, up, low, ax, fig
 
 
 def pplot(*args, ax=None, fig=None, legend=True, title=None, grid=True, **kargs):
-
     """
     Function that plots pyomo Variable or Parameter
 
@@ -127,22 +153,31 @@ def pplot(*args, ax=None, fig=None, legend=True, title=None, grid=True, **kargs)
         >>> lines = pplot(m.v, m.z, m.w, title='test', Marker='x')
     """
 
-    ncol            = kargs.pop('ncol', 4)
-    loc             = kargs.pop('loc', 'lower left')
-    bbox_to_anchor  = kargs.pop('bbox_to_anchor',(0, 1.02, 1, 0.2))
-    mode            = kargs.pop('mode', "expand")
+    ncol = kargs.pop('ncol', 4)
+    loc = kargs.pop('loc', 'lower left')
+    bbox_to_anchor = kargs.pop('bbox_to_anchor', (0, 1.02, 1, 0.2))
+    mode = kargs.pop('mode', "expand")
 
     lines = []
-    ld, ax, fig = _pplot(args[0], ax=ax, fig=fig, **kargs)
+    ld, up, low, ax, fig = _pplot(args[0], ax=ax, fig=fig, **kargs)
+    if up is not None:
+        lines.append(up)
+    if low is not None:
+        lines.append(low)
+
     lines.append(ld)
 
     for var in args[1:]:
-        ld, ax, fig = _pplot(var, ax=ax, fig=fig, **kargs)
+        ld, up, low, ax, fig = _pplot(var, ax=ax, fig=fig, **kargs)
         lines.append(ld)
+        if up is not None:
+            lines.append(up)
+        if low is not None:
+            lines.append(low)
 
     if legend:
         ax.legend(bbox_to_anchor=bbox_to_anchor, loc=loc,
-                        mode=mode, borderaxespad=0, ncol=ncol)
+                  mode=mode, borderaxespad=0, ncol=ncol)
     if title is not None:
         ax.set_title(title)
     if grid:
@@ -228,6 +263,91 @@ def get_doc_2(bloc):
         doc += '=============== ===================================================================\n\n'
 
     return doc
+
+
+from pyomo.environ import Constraint, Var
+
+
+def fix_binary(model):
+    """
+    Fix binary variables to their values
+
+    :return:
+    """
+
+    from pyomo.environ import RealSet
+
+    for u in model.component_objects(Var):
+        if u.is_indexed():
+            for ui in u.itervalues():
+                if ui.is_binary():
+                    ui.fix(ui.value)
+                    ui.domain = RealSet([0, 1])
+        else:
+            if u.is_binary():
+                u.fix(u.value)
+                u.domain = RealSet([0, 1])
+
+
+def unfix_binary(model):
+    """
+    Unfix binary variables
+
+    :return:
+    """
+    for u in model.component_objects(Var):
+        if u.is_indexed():
+            for ui in u.itervalues():
+                if ui.is_binary():
+                    ui.unfix()
+        else:
+            if u.is_binary():
+                u.unfix()
+
+
+def get_duals(model, dual_name='dual'):
+    """
+    Return dual coefficient of LP abstract model.
+
+    :param str dual_name: name of the Suffix
+    :return : Dual coefficient (DataFrame)
+    """
+    from pandas import DataFrame, concat
+    from pyomo.environ import Constraint
+    df = DataFrame()
+
+    assert hasattr(model, dual_name), f'"{model.name}" does not have attribute named "{dual_name}".'
+
+    for cst in model.component_objects(Constraint, active=True):
+        if cst.is_indexed():
+            s = DataFrame(
+                {cst.getname(fully_qualified=True) + '_' + dual_name: {i: model.component(dual_name)[c]
+                                                                       for (i, c) in cst.iteritems()}})
+            df = concat([df, s], axis=1)
+        else:
+            Warning('Trying to get dual coefficient from a non-index variable. Not Implemented Yet')
+    return df
+
+
+def get_slack(model):
+    """
+    Return slack variables values for all
+    the active constraints of a abstract model.
+
+    :return: DataFrame
+    """
+
+    from pandas import DataFrame, Series, concat
+
+    df = DataFrame()
+    for c in model.component_objects(Constraint, active=True):
+        if c.is_indexed():
+            s1 = Series({i: c[i].lslack() for i in c.__iter__()})
+            s2 = Series({i: c[i].uslack() for i in c.__iter__()})
+            df_c = DataFrame(
+                {c.getname(fully_qualified=True).replace('.', '_') + '_ls': s1, c.getname() + '_us': s2})
+            df = concat([df, df_c], axis=1)
+    return df
 
 
 if __name__ == '__main__':
