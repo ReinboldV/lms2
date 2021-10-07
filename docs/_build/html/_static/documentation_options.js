1388341d2ac5e7fdf8f557d2c96dedf6f9aaa37a
Vincent Reinbold <vincent.reinbold@geeps.centralesupelec.fr>
2021-10-07 10:27:13 +0200

    update docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1+2.g10d0b63.dirty',
+    VERSION: '1.1+4.g396374b.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='lms2',
     version=versioneer.get_version(),
     description='Library for Linear Modelling of Energetic Systems',
-    author='Vincent Reinbold',
+    author='Vincent Reinbold, Celia Masternak',
     author_email='vincent.reinbold@gees.centralesupelec.fr',
     license='BSD',
     packages=['lms2',
@@ -21,5 +21,6 @@ setup(
               'lms2.social',
               'lms2.thermal',
               'lms2.electric'],
-    classifiers=["Programming Language :: Python :: 3.6", "Programming Language :: Python :: 3.7"],
+    classifiers=["Programming Language :: Python :: 3.6", "Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.9", "Programming Language :: Python :: 3.9"],
 )
